c4e5638a635d75f3c7ee0dbf1ee52ea4be343866
Ryan Hartlage <ryanplusplus@gmail.com>
2023-02-19 18:44:23 -0500

    Added support for debugging with mspdebug

--- mspdebug.mk
+++ mspdebug.mk
@@ -1,3 +1,10 @@
+.PHONY: $(BUILD_DIR)/debug-server
+$(BUILD_DIR)/debug-server:
+	@echo "#!/bin/bash" > $@
+	@echo "echo '\nInfo : Listening on port 50000 for gdb connection\n'" >> $@
+	@echo "mspdebug $(MSPDEBUG_DEVICE) \"gdb 50000\"" >> $@
+	@chmod +x $@
+
 .PHONY: upload
 upload: $(BUILD_DIR)/$(TARGET).hex
 	@mspdebug -q $(MSPDEBUG_DEVICE) "prog $<"
@@ -5,3 +12,6 @@ upload: $(BUILD_DIR)/$(TARGET).hex
 .PHONY: erase
 erase:
 	@mspdebug -q $(MSPDEBUG_DEVICE) "erase"
+
+.PHONY: debug-deps
+debug-deps: $(BUILD_DIR)/debug-server $(BUILD_DIR)/$(TARGET).hex
